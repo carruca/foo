f4b4ec79f30b9db2ee5edb4bb763efbef7328597
Tomas Sierra paradinas <tsierra-@c1r11s1.42madrid.com>
2021-02-26 17:13:51 +0100

    Add extern environ var

--- main_list.c
+++ main_list.c
@@ -6,19 +6,26 @@
 /*   By: tsierra- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 15:11:12 by tsierra-          #+#    #+#             */
-/*   Updated: 2021/02/26 15:26:33 by tsierra-         ###   ########.fr       */
+/*   Updated: 2021/02/26 17:13:06 by tsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+extern char **environ;
 
 int	main()
 {
 	t_list	*list;
 	char	str[] = "hello world";
+	char	**nv = environ;
 
 	list = ft_lstnew(str);
 	printf("%s\n", list->content);
+	while (*nv)
+	{
+		printf("%s\n", *nv);
+		nv++;
+	}
 	return (0);
 }
