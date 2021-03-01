ad5c1545c634588e86bf6d51c46592050e026c40
Tomas Sierra paradinas <tsierra-@c1r11s1.42madrid.com>
2021-03-01 18:56:03 +0100

    Add free_2d funtion

--- free_2d.c
+++ free_2d.c
@@ -1,31 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   free_2d.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsierra- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/26 14:54:39 by tsierra-          #+#    #+#             */
-/*   Updated: 2021/03/01 18:42:13 by tsierra-         ###   ########.fr       */
+/*   Created: 2021/03/01 18:32:31 by tsierra-          #+#    #+#             */
+/*   Updated: 2021/03/01 18:43:26 by tsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-# include <stdio.h>
-# include "chunk.h"
-
-typedef struct		s_list
+void	free_2d(char **ptr)
 {
-	void			*content;
-	struct s_list	*next;
-}					t_list;
-
-char	**ft_split(char const *s, char c);
-void	free_2d(char **ptr);
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_back(t_list **list, t_list *new);
-
-#endif
+	while (*ptr)
+	{
+		free(*ptr);
+		ptr++;
+	}
+	free(*ptr);
+	ptr = NULL;
+}
