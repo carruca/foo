4a6cc40e62cee92139423749b07a1853775039e5
carruca <tomtomsierra@gmail.com>
2021-04-12 01:52:37 +0200

    Add pipe and redir options to try

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int	main(int argc, char **argv)
 	printf("I am the parent process\n");
 	return (0);
 }*/
-
+/*
 void	error_handling(void)
 {
 	char *error;
@@ -57,7 +57,6 @@ int	main(int argc, char **argv)
 	//free(buf);
 	write(1, cwd, strlen(cwd));
 	write(1, "\n", 1);
-	/*
 	dirp = opendir(".");
 
 	if (dirp == NULL)
@@ -69,6 +68,59 @@ int	main(int argc, char **argv)
 	{
 		printf("%s\n", entry->d_name);
 	}
-	*/
 	return (0);
+}*/
+
+int	main(int argc, char **argv, char **environ)
+{
+	int	stdin_tmp;
+	int	stdout_tmp;
+	int	fdin;
+	int	fdout;
+	int	child_pid;
+	int	i;
+	int	cmd_num;
+	int	fd[2];
+	int	status;
+
+	stdin_tmp = dup(stdin);
+	stdout_tmp = dup(stdout);
+	if (argc == 3)
+		fdin = open(argv[1], O_RDONLY);
+	else
+		fdin = dup(stdin_tmp);
+	i = 0;
+	cmd_num = 2;
+	while (i < cmd_num)
+	{
+		dup2(fdin, stdin);
+		close(fdin);
+		if (i == cmd_num - 1)
+		{
+			if (argc == 3)
+				fdout = open(argv[2], O_WRONLY);
+			else
+				fdout = dup(stdout_tmp);
+		}
+		else
+		{
+			pipe(fd);
+			fdin = fd[0];
+			fdout = fd[1];
+		}
+		dup2(fdout, 1);
+		close(fdout);
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			execve("/bin/ls", argv, environ);
+			exit(0);
+		}
+		i++;
+	}
+	dup2(stdin_tmp, stdin);
+	dup2(stdout_tmp, stdout);
+	close(stdin_tmp);
+	close(stdout_tmp);
+	waitpid(child_pid, &status, 0);
 }
